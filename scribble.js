4f6a167b460c6fff395c4fd7cf8347ed80330053
Ryan McGarvey <rmcgarve@live.unc.edu>
2013-10-28 00:51:43 -0400

    textbox deletes old chars

--- scribble.js
+++ scribble.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-	/*$("#textbox").onchange(function(e){
+	
+	$("#textbox").keyup(function(e){
 		if ($("#textbox").val().length>1) {
 			var temp=$("#textbox").val();
 			$("#textbox").val(temp.substring((temp.length-1),(temp.length)));
 		}
-	});*/
-	
+	});
+		
 	$("#scribble").click(function(e){
 		$(".cellRow").show();
 		$("#matching").hide();
