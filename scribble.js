fb1680361ab59e927a6bc3106b35bf4ce9182202
Ben Koury <lightcube182@gmail.com>
2013-11-25 15:29:54 -0500

    More testig

--- scribble.js
+++ scribble.js
@@ -70,7 +70,7 @@ $(document).ready(function(){
 		}
 	});
 	
-	$("#textbox").keypress(function() {
+	$("#textbox").trigger(function() {
 		if (doGetCaretPosition(document.getElementById('textbox')) == 0) {
 			if (randLetterKeys[0] == correctAnswer) {
 				$("div.infoBar").text("That's right!!!");
